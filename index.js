f2add402c6e4a3823dbb1812e43272288e52ee9f
tal shachar <tal.shachar@buzzfeed.com>
2017-12-03 00:46:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function endGame() {
   clearInterval(gameInterval)
 
   ROCKS.forEach(function(rock) { rock.remove() })
-  document.removeEventListener('keydown', moveDodger)
+  document.removeEventListener('keydown',moveDodger)
 
   START.innerHTML = 'Play again?'
   START.style.display = 'inline'
