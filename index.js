8ea9d7a95d5866aa43d9897b20aee3d340ab3381
tal shachar <tal.shachar@buzzfeed.com>
2017-12-03 00:51:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function endGame() {
 
   ROCKS.forEach(function(rock) { rock.remove() })
 
-  document.removeEventListener('keydown', moveDodger)
+  window.removeEventListener('keydown', moveDodger)
 
   return alert('YOU LOSE!')
 }
